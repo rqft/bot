3babe90957c7fcbf09e00276be7c50369c2cf229
Arcy <higharcswastaken@gmail.com>
2021-01-30 19:05:17 -0600

    fix token

--- dist/token.js
+++ dist/token.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.token = void 0;
-exports.token = "NzYwMTQzNjE1MTI0NDM5MDQw.X3Hw6A.XAckrRrCY5Tk3TtHbSm6eGg_gN4";
+exports.token = "NzYwMTQzNjE1MTI0NDM5MDQw.X3Hw6A.-jU_Y8MKRsTXtWA6V0fMaBoniqI";
