1afe209ed2f01dbb3f13e4941e8ba1d2b0fc22d7
Arcy <higharcswastaken@gmail.com>
2021-01-31 10:49:33 -0600

    add links to about

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -16,9 +16,7 @@ module.exports = {
               (await client.fetchApplication()).owner
             } made with [DiscordJS](https://discord.js.org/#/) and a stupid idea.
 
-You can invite the bot to your server [here](https://discord.com/api/oauth2/authorize?client_id=${
-              (await client.fetchApplication()).id
-            }&permissions=8&scope=bot)`,
+You can invite the bot to your server [here]`,
           },
         ],
       })
