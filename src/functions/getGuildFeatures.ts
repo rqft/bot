3baae4bf7088e7badcb3ae78594ce430d44a12aa
Arcy <higharcswastaken@gmail.com>
2021-02-01 07:51:38 -0600

    add 'bot server' guild flag

--- src/functions/getGuildFeatures.ts
+++ src/functions/getGuildFeatures.ts
@@ -1,10 +1,22 @@
 import { Guild } from "discord.js";
+import { config } from "../config";
 import { guildFeatureMap } from "../maps/guildFeature";
 
 export function getGuildFeatures(guild: Guild, showIcons: boolean = true) {
   const feat = [];
+  if (guild.id == config.__global.guildId)
+    feat.push(
+      `${
+        showIcons ? "<:IconGui_OwnerCrown:799657143719952415>" : ""
+      } Bot Server`
+    );
+
   if (guild.me) {
-    feat.push("<:Hallucinate:800092998590529557> Uses Hallucinate <3");
+    feat.push(
+      `${
+        showIcons ? "<:Hallucinate:800092998590529557>" : ""
+      } Uses Hallucinate <3`
+    );
   }
   guild.features.forEach((element) => {
     feat.push(
