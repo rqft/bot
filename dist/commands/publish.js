9d3add468cfd9e9f8b3d39f90f4e911d23fd072a
Arcs <higharcswastaken@gmail.com>
2021-02-02 14:20:39 -0600

    g

--- dist/commands/publish.js
+++ dist/commands/publish.js
@@ -9,7 +9,9 @@ module.exports = {
     usesArgs: true,
     usage: "<message: text>",
     async run(message, args) {
-        child_process_1.exec(`git add .; git commit -m ${args.join(" ")}`);
-        await message.channel.send(`:white_check_mark: Published @ https://github.com/arcy-at/Hallucinate with message \`${args.join(" ")}\``);
+        await child_process_1.exec(`git commit -m ${args.join(" ")}`, (_, stdout, _stderr) => {
+            message.channel.send([stdout, _stderr].join("\n\n"), { code: "bash" });
+        });
+        await message.channel.send(":white_check_mark: Deployed @ https://github.com/arcy-at/Hallucinate");
     },
 };
--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -9,11 +9,12 @@ module.exports = {
   usesArgs: true,
   usage: "<message: text>",
   async run(message, args) {
-    exec(`git add .; git commit -m ${args.join(" ")}`);
+    await exec(`git commit -m ${args.join(" ")}`, (_, stdout, _stderr) => {
+      message.channel.send([stdout, _stderr].join("\n\n"), { code: "bash" });
+    });
     await message.channel.send(
-      `:white_check_mark: Published @ https://github.com/arcy-at/Hallucinate with message \`${args.join(
-        " "
-      )}\``
+      ":white_check_mark: Deployed @ https://github.com/arcy-at/Hallucinate"
     );
   },
 } as ICommand;
+//
