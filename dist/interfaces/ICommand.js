b34b21b0b968ac777fdaadcdd2fe9a92e83ebfd1
Arcy <higharcswastaken@gmail.com>
2021-02-04 08:57:13 -0600

    nope nevermind

--- dist/interfaces/ICommand.js
+++ dist/interfaces/ICommand.js
@@ -1,15 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeCommandUsage = exports.parseArgument = void 0;
-function parseArgument(arg) {
-    const reqL = arg.required ? "<" : "[";
-    const reqR = arg.required ? ">" : "]";
-    return `${reqL}${arg.name}: ${arg.type}${reqR}`;
-}
-exports.parseArgument = parseArgument;
-function makeCommandUsage(usedPrefix, command, args) {
-    return `${usedPrefix}${command.name} ${args
-        .map((e) => parseArgument(e))
-        .join(" ")}`;
-}
-exports.makeCommandUsage = makeCommandUsage;
