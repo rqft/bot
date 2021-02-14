d661e0352caeb5e0f1e61a0d1ab1c7d8cf553c08
Arcy <higharcswastaken@gmail.com>
2021-02-13 19:22:53 -0600

    funny sex bot got someone ratelimited

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.commandFiles = exports.commands = exports.client = void 0;
 const discord_js_1 = __importDefault(require("discord.js"));
 const config_1 = require("./config");
-const formatID_1 = require("./functions/formatID");
 const commandHandler_1 = require("./handlers/commandHandler");
 const discordjsError_1 = require("./handlers/discordjsError");
 const fetchCommandFiles_1 = require("./handlers/fetchCommandFiles");
@@ -28,22 +27,6 @@ exports.client.once("ready", () => {
 });
 exports.client.on("error", (e) => discordjsError_1.discordjsError(e));
 exports.client.on("message", async (message) => {
-    const sexes = message.content.match(/sex/gi);
-    if (sexes) {
-        if (message.author !== exports.client.user) {
-            message.author.send(`No sex :bangbang:`);
-            await message.react("😳");
-        }
-        config_1.config.global.sexAlarm.forEach(async (e) => {
-            if (exports.client.user == message.author && message.channel.type == "dm")
-                return;
-            (await exports.client.channels.fetch(e))
-                .send(`...`)
-                .then((e) => e.edit(`${message.author} ${formatID_1.formatID(message.author.id)} has **sexed** __${sexes.length} time${sexes.length == 1 ? "" : "s"}__ in ${message.guild ? message.channel : "DMs"} ${formatID_1.formatID(message.channel.id)} ${message.guild && message.guild.id !== config_1.config.global.guildId
-                ? `on \`${message.guild.name}\` ${formatID_1.formatID(message.guild.id)}`
-                : ""}`));
-        });
-    }
     await commandHandler_1.commandHandler(message);
 });
 exports.client.login(config_1.config.bot.token);
