45c7defa26ec36132889c53e20aa2d87e1133214
Arcs <higharcswastaken@gmail.com>
2021-02-25 08:06:14 -0600

    cate - make loading symbol appear if count > 10

--- dist/commands/cate.js
+++ dist/commands/cate.js
@@ -9,11 +9,13 @@ module.exports = {
     usesArgs: false,
     description: "cats!",
     async run(message, args) {
+        const ret = await message.reply("<a:IconGui_Typing:798624244351107092>");
         const count = parseInt(args[0] ?? "1");
         const urls = [];
         for (let i = 0; i < count; i++) {
             urls.push((await api_1.api("http://aws.random.cat/meow", "json")).file);
         }
+        await ret.delete();
         const urlGroups = arrayToChunks_1.arrayToChunks(urls, 5);
         urlGroups.forEach((url) => {
             message.channel.send(url.join("\n"));
--- src/commands/cate.ts
+++ src/commands/cate.ts
@@ -1,6 +1,7 @@
 import { api } from "../functions/api";
 import { arrayToChunks } from "../functions/arrayToChunks";
 import { ICommand } from "../interfaces/ICommand";
+import { CustomEmojis } from "../maps/customEmojis";
 
 module.exports = {
   name: "cate",
@@ -9,12 +10,13 @@ module.exports = {
   usesArgs: false,
   description: "cats!",
   async run(message, args) {
+    const ret = await message.reply(CustomEmojis.GUI_TYPING);
     const count = parseInt(args[0] ?? "1");
     const urls = [];
     for (let i = 0; i < count; i++) {
       urls.push((await api("http://aws.random.cat/meow", "json")).file);
     }
-
+    await ret.delete();
     const urlGroups = arrayToChunks(urls, 5);
     urlGroups.forEach((url) => {
       message.channel.send(url.join("\n"));
