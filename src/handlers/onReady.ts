15f7835951e0d1c8039ebe18b9cfe99d0d099bff
Arcs <higharcswastaken@gmail.com>
2021-02-26 11:21:41 -0600

    got hoob

--- src/handlers/onReady.ts
+++ src/handlers/onReady.ts
@@ -1,19 +1,19 @@
-import { VoiceChannel } from "discord.js";
-import { client } from "..";
-import { config } from "../config";
-import { leaveBlacklistedGuilds } from "../logs/leaveBlacklistedGuilds";
-import { makeConsoleDeployMessage } from "./makeConsoleDeployMessage";
-import { makeDeployMessage } from "./makeDeployMessage";
+// import { VoiceChannel } from "discord.js";
+// import { client } from "..";
+// import { config } from "../config";
+// import { leaveBlacklistedGuilds } from "../logs/leaveBlacklistedGuilds";
+// import { makeConsoleDeployMessage } from "./makeConsoleDeployMessage";
+// import { makeDeployMessage } from "./makeDeployMessage";
 export async function onReady() {
   const start = Date.now();
-  makeConsoleDeployMessage();
-  makeDeployMessage(config.logs.starts.keys);
-  const ch = client.channels.cache.get(
-    config.bot.presence.voiceChannel
-  ) as VoiceChannel;
-  const connection = await ch.join();
-  connection.setSpeaking("SPEAKING");
+  // makeConsoleDeployMessage();
+  // makeDeployMessage(config.logs.starts.keys);
+  // const ch = client.channels.cache.get(
+  //   config.bot.presence.voiceChannel
+  // ) as VoiceChannel;
+  // const connection = await ch.join();
+  // connection.setSpeaking("SPEAKING");
 
   console.log(`took ${Date.now() - start}ms to complete`);
-  leaveBlacklistedGuilds();
+  // leaveBlacklistedGuilds();
 }
